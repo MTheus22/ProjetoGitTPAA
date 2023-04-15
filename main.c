e87fdbee50ddebd13b1259b19a0c429791a7b4fc
Luiz_Eduardo <luiz.dias@perpetuo.g12.br>
2023-04-15 15:25:11 -0300

    Adicionado Func Hash, falta arrumar

--- main.c
+++ main.c
@@ -1,7 +1,47 @@
 #include "branch.h"
 #include <stdio.h>
 #include <stdlib.h>
+char *gera_hash(gancho *cabeca, char *hash)
+{
+    char string[50];
+    int resultado = 0;
+    for (int i=0; i < 5; i++)
+    {
+        string[2 * i] = 48 + (rand() % 9);
+        string[2 * i + 1] = 65 + (rand() % 6);
+    }
+    resultado = verifica_combinacao(string, cabeca);
+    if (resultado == ERRO)
+        hash = gera_hash(cabeca, hash);
+    else
+    {
+        strcpy(hash, string);
+        return hash;
+    }
+}
 
+int verifica_combinacao(char *string, gancho *cabeca)
+{
+    lista *atual = cabeca->primeiro;
+    int resultado = 0;
+    if (cabeca->primeiro == NULL)
+    {
+        return CORRETO;
+    }
+    else
+    {
+        while (atual != NULL)
+        {
+            resultado = strcmp(atual->hash, string);
+            if (resultado == 0)
+            {
+                return ERRO;
+            }
+            atual = atual->proximo;
+        }
+        return CORRETO;
+    }
+}
 int main(void) {
   head *h;
   branch *master;
@@ -19,5 +59,6 @@ int main(void) {
   printf("Hash do commit: %d\n", h->b->c->endereco);
   printf("Mensagem do commit: %c\n", h->b->c->mensagem);
 
+  
   return 0;
 }
